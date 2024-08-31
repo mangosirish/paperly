37ce1351927de2240fbc04aefc942142f0a88592
Ricardo_Montoya <drmontoya502@gmail.com>
2024-08-31 13:01:32 -0600

    Templ was implemented

--- app/go.mod
+++ app/go.mod
@@ -1,8 +1,11 @@
 module github.com/mangosirish/paperly
 
-go 1.18
+go 1.21
+
+toolchain go1.23.0
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/a-h/templ v0.2.771
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
 )
--- app/main.go
+++ app/main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-    "github.com/mangosirish/paperly/db"
-    "github.com/mangosirish/paperly/handlers"
+	"github.com/mangosirish/paperly/db"
+	"github.com/mangosirish/paperly/handlers"
+	"github.com/mangosirish/paperly/templates"
 )
 
 func main() {
-    db.InitDB()
-    defer db.CloseDB()
+	db.InitDB()
+	defer db.CloseDB()
 
-    http.HandleFunc("/articles", handlers.GetArticles)
-    http.HandleFunc("/people", handlers.GetPeople)
-    http.HandleFunc("/authors", handlers.GetAuthors)
-    http.HandleFunc("/academic-careers", handlers.GetAcademicCareers)
-    http.HandleFunc("/faculties", handlers.GetFaculties)
-    http.HandleFunc("/institutions", handlers.GetInstitutions)
-    http.HandleFunc("/student-social-services", handlers.GetStudentSocialServices)
-    http.HandleFunc("/journals", handlers.GetJournals)
-    http.HandleFunc("/specialties", handlers.GetSpecialties)
+	// Frontend
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		templates.IndexPage().Render(r.Context(), w)
+	})
 
-    fmt.Println("Servidor en ejecución en http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	// API
+	http.HandleFunc("/articles", handlers.GetArticles)
+	http.HandleFunc("/people", handlers.GetPeople)
+	http.HandleFunc("/authors", handlers.GetAuthors)
+	http.HandleFunc("/academic-careers", handlers.GetAcademicCareers)
+	http.HandleFunc("/faculties", handlers.GetFaculties)
+	http.HandleFunc("/institutions", handlers.GetInstitutions)
+	http.HandleFunc("/student-social-services", handlers.GetStudentSocialServices)
+	http.HandleFunc("/journals", handlers.GetJournals)
+	http.HandleFunc("/specialties", handlers.GetSpecialties)
+
+	fmt.Println("Servidor en ejecución en http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
