a597cd92edc85828ee991f9144a6f0dc9d8725ea
Ricardo_Montoya <drmontoya502@gmail.com>
2024-11-30 22:12:34 -0600

    Update: Forms defined v2

--- app/static/js/forms_logic.js
+++ app/static/js/forms_logic.js
@@ -86,7 +86,7 @@ function createFormHandler() {
 	};
 }
 
-function restoreProfileCounter(){
+function restoreProfileCounter() {
 	profileCounter = 0;
 }
 
@@ -96,46 +96,52 @@ let debounceTimer = null;
 
 // Función para buscar autores
 function searchAuthors(searchQuery) {
-    debounceTimer = setTimeout(() => {
-        let apiRoute = searchQuery.trim() !== '' ? `/people/search/${searchQuery}` : '/people';
-        fetch(apiRoute)
-            .then(response => response.json())
-            .then(data => {
-                const ulElement = document.getElementById('author_search_results');
-                ulElement.innerHTML = '';
+	debounceTimer = setTimeout(() => {
+		let apiRoute = searchQuery.trim() !== '' ? `/people/search/${searchQuery}` : '/people';
+		fetch(apiRoute)
+			.then(response => response.json())
+			.then(data => {
+				const ulElement = document.getElementById('author_search_results');
+				ulElement.innerHTML = '';
 
-                data.forEach(author => {
-                    const li = document.createElement('li');
-                    li.textContent = `${author.first_name} ${author.first_surname}`;
-                    li.setAttribute('x-on:click', `setResultAuthor(${JSON.stringify(author)})`);
-                    ulElement.appendChild(li);
-                });
-            })
-            .catch(error => {
-                console.error('Error al hacer la solicitud:', error);
-            });
-    }, 500);
+				data.forEach(author => {
+					const li = document.createElement('li');
+					li.textContent = `${author.first_name} ${author.first_surname}`;
+					li.setAttribute('x-on:click', `setResultAuthor(${JSON.stringify(author)})`);
+					ulElement.appendChild(li);
+				});
+			})
+			.catch(error => {
+				console.error('Error al hacer la solicitud:', error);
+			});
+	}, 500);
 }
 
 // Función para buscar journals
 function searchJournals(searchQuery) {
-    debounceTimer = setTimeout(() => {
-        let apiRoute = searchQuery.trim() !== '' ? `/journals/search/${searchQuery}` : '/journals';
-        fetch(apiRoute)
-            .then(response => response.json())
-            .then(data => {
-                const ulElement = document.getElementById('journal_search_results');
-                ulElement.innerHTML = '';
+	debounceTimer = setTimeout(() => {
+		let apiRoute = '/journals/details';
+		fetch(apiRoute)
+			.then(response => response.json())
+			.then(raw_data => {
+				const data = searchQuery.trim()
+					? raw_data.filter(item =>
+						item.Ejemplar?.toLowerCase().includes(searchQuery.toLowerCase())
+					)
+					: raw_data;
+					console.log(raw_data)
+				const ulElement = document.getElementById('journal_search_results');
+				ulElement.innerHTML = '';
 
-                data.forEach(journal => {
-                    const li = document.createElement('li');
-                    li.textContent = `${journal.title} (${journal.publication_date})`;
-                    li.setAttribute('x-on:click', `setResultModeJournal(${JSON.stringify(journal)})`);
-                    ulElement.appendChild(li);
-                });
-            })
-            .catch(error => {
-                console.error('Error al hacer la solicitud:', error);
-            });
-    }, 500);
+				data.forEach(journal => {
+					const li = document.createElement('li');
+					li.textContent = `${journal.Ejemplar} (${journal.Nombre})`;
+					li.setAttribute('x-on:click', `setResultModeJournal(${JSON.stringify(journal)})`);
+					ulElement.appendChild(li);
+				});
+			})
+			.catch(error => {
+				console.error('Error al hacer la solicitud:', error);
+			});
+	}, 500);
 }
